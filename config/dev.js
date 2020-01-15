63f23cab022fd29e31ca13a24f2776c4b97ace09
Ahmed Khaled <36384512+AhmedKhaledRe@users.noreply.github.com>
2020-01-15 20:33:55 +0200

    first commit

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+    DB_URI: "mongodb+srv://ahYelp:kJFf67OPS3LUTZGX@book-6yd0x.mongodb.net/test",
+    //SECRET: process.env.SECRET,
+    SECRET: "jkfdjkgdljkgl",
+    //AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    //AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    //STRIPE_SK: process.env.STRIPE_SK
+    STRIPE_SK: "sk_test_rQwou9zCCakPBDc5GfQg0kpa00WdDGm6EJ"
+  };
